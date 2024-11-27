cb039b78b25267628f48c1e5fbc63e7143c33adc
Alex <alexandercromar@gmail.com>
2024-11-27 11:12:26 -0700

    cleaned the advertisement

--- server/gatt_counter.c
+++ server/gatt_counter.c
@@ -89,7 +89,7 @@ const uint8_t adv_data[] = {
     // Flags general discoverable
     0x02, BLUETOOTH_DATA_TYPE_FLAGS, APP_AD_FLAGS,
     // Name
-    11, BLUETOOTH_DATA_TYPE_COMPLETE_LOCAL_NAME, 'A', 'L', 'S', 't', 'e', 's', 't', 'B', 'L', 'E',
+    11, BLUETOOTH_DATA_TYPE_COMPLETE_LOCAL_NAME, 'A', 'L', 'S',' ', 't', 'e', 's', 't',' ',' ',
     // Incomplete List of 16-bit Service Class UUIDs -- FF10 - only valid for testing!
     0x03, BLUETOOTH_DATA_TYPE_INCOMPLETE_LIST_OF_16_BIT_SERVICE_CLASS_UUIDS, 0x10, 0xff,
 };
